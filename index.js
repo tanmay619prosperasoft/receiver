81e9297cede85d0d1cd23376dd13e6731aaec51c
tanmay619prosperasoft <tanmay@prosperasoft>
2023-10-11 15:36:46 +0530

    file updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
