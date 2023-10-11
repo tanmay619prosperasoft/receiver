6c7fc56f3048a863bd1b2b541b71ba4ce020e83b
tanmay619prosperasoft <tanmay@prosperasoft>
2023-10-11 14:16:06 +0530

    created new branch

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json())
+app.use(express.json());
+
+app.use('/',(req,res)=>{
+  res.send("Hello")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
